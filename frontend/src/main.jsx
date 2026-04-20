acb41ef9deff9114d24fac6e02983f1473fc7176
jze0725 <jze0725@gmail.com>
2026-04-20 21:55:25 +0800

    Add Job Detail Form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HRSignUp from './HR_SignUp.jsx'
-import './index.css'
+import './styles/global.css'
+import './styles/common.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
